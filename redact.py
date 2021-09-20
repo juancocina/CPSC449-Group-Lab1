8e718039c4440b36c5c3e98c98b865b5d1b6dc25
juancocina <juan_cocina@csu.fullerton.edu>
2021-09-20 15:44:20 -0700

    Script now retrieves json and parses it for url

--- redact.py
+++ redact.py
@@ -7,6 +7,7 @@ import sys
 import ssl
 import http.client
 import json
+import urllib
 
 # Display script use
 print('Usage: redact URL')
@@ -18,11 +19,28 @@ f = sys.argv[1]
 
 # Use http.client.HTTPSConnection with the Accept: application/json request header
 h1 = http.client.HTTPSConnection("foaas.com")
-
 headers = {"Accept": "application/json"}
 h1.request("GET", f, "", headers)
 
 # checking response from foaas
 res = h1.getresponse()
 if res.status == 200:
-    print(res.read())
+    print("Successfully connected")
+
+    # attempting to use json
+    incoming = res.read()
+    data = json.loads(incoming.decode())
+    message = data['message']
+    print("Raw data: ", data)
+    print("'Message' parameter: ", message)
+    parsed_message = urllib.parse.quote(message)
+    print("Parsed: ", parsed_message)
+
+    # attempting to connect to purgomalum.com
+    h2 = http.client.HTTPSConnection("purgomalum.com")
+    h2.request("GET", "/")
+
+else:
+    # exit program
+    print("Check URL")
+    exit(1)
