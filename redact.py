50df88b78e2ecdab02ff1edfcd1ea0245f0f1327
juancocina <juan_cocina@csu.fullerton.edu>
2021-09-21 21:18:19 -0700

    You can now access foaas' messages through url changes

--- redact.py
+++ redact.py
@@ -24,11 +24,42 @@ def servercall(p, s):
             self.send_response(200)
             self.send_header('Content-Type', 'text/html; charset=utf-8')
             self.end_headers()
-    
-            payload = '<h1>'+pay+'</h1>'
-            subload = '<h2>'+subt+'</h2>'
+
+            # Grab path from localhost and open connection to foaas
+            route = self.path
+            h3 = http.client.HTTPSConnection("foaas.com")
+            h3.request("GET", route, "", headers)
+
+            # check connection and proceed
+            res = h3.getresponse()
+            if res.status == 200:
+                print('')
+                print("h3 successfully connected")
+
+                # convert messages to json
+                incoming = res.read()
+                data = json.loads(incoming.decode())
+                message = data['message']
+                subtitle = data['subtitle']
+                parsed_message = urllib.parse.quote(message)
+
+                # connecting to purgomalum.com, retreiving message, converting to json, printing
+                url = "https://www.purgomalum.com/service/json?text=" + parsed_message
+                h4 = urllib.request.urlopen(url)
+                censored = h4.read()
+                censored_data = json.loads(censored.decode())
+                # prints the final censored message
+                final_message = censored_data['result']
+                message = f"""
+                    {final_message}
+                    {subtitle}
+                """
+
+            # payload = '<h1>'+pay+'</h1>'
+            # subload = '<h2>'+subt+'</h2>'
+            payload =  '<h1>' + message + '</h1>'
             self.wfile.write(payload.encode('utf-8'))
-            self.wfile.write(subload.encode('utf-8'))
+            # self.wfile.write(subload.encode('utf-8'))
             
     with socketserver.TCPServer(("", PORT), ExHTTPRequestHandler) as httpd:
         print("serving at port", PORT)
@@ -36,8 +67,10 @@ def servercall(p, s):
 
 # Display script use
 print('Usage: redact URL')
-print('Please include a FOAAS url')
-print('Example: "/because/ProfAvery" ')
+if len(sys.argv) == 1:
+    print('Please include a FOAAS url')
+    print('Example: "/because/ProfAvery" ')
+    exit(1)
 
 # Take the url from argv using sys.argv
 f = sys.argv[1]
@@ -50,18 +83,19 @@ h1.request("GET", f, "", headers)
 # checking response from foaas
 res = h1.getresponse()
 if res.status == 200:
-    print("Successfully connected")
+    print("Successfully connected to foaas.com")
 
     # converting incoming message to json and parsing
     incoming = res.read()
     data = json.loads(incoming.decode())
     message = data['message']
     subtitle = data['subtitle']
-    print("Raw data: ", data)
-    print("'Message' parameter: ", message)
+    # commenting out print statements used for testing
+    # print("Raw data: ", data)
+    # print("'Message' parameter: ", message)
     parsed_message = urllib.parse.quote(message)
-    print("Parsed: ", parsed_message)
-    print('')
+    # print("Parsed: ", parsed_message)
+    # print('')
 
     # connecting to purgomalum.com, retreiving message, converting to json, printing
     url = "https://www.purgomalum.com/service/json?text=" + parsed_message
@@ -78,6 +112,9 @@ if res.status == 200:
     
     servercall(p=mes, s=sub)
 
+    # close the command line connections (incase they didn't close themselves
+    h1.close()
+    h2.close()
 else:
     # exit program
     print("Check URL")
